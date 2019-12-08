07c1d1d99f3fa414980b1e666317669eb3c933f4
Jan Hapis <57375955+janhapis@users.noreply.github.com>
2019-12-08 21:00:48 +0800

    Add files via upload

---
+++ Problem_1_Python.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+def function(n):
+    if n <= 9:
+        return (n**2) - 7
+    elif n >= 10:
+        return function(n-10)
+
+x = []
+y = []
+
+for character in range(0,100):
+    x.append(character)
+    y.append(function(character))
+    
+plt.stem(x,y, use_line_collection = True)    
+plt.title('Graph of f(n)')
+plt.xlabel('X-Axis')
+plt.ylabel('Y-Axis')
